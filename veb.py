1679ac3f435df19d3ef12c0832e500e5293ad96b
Tihaya_mishka <ayashkoldina@yandex.ru>
2024-04-23 03:00:16 +0300

    aiogrProbn

---
+++ veb.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('7111234144:AAEM82iAmq7uJGruhIUL64NsEdFUvmhmjho')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Open web', web_app=WebAppInfo(url='https://itproger.com')))
+    await message.answer("Hi, my friend!", reply_markup=markup)
+
+executor.start_polling(dp)
